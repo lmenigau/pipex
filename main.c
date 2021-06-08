0bc1434823ed7d2e33def448a8eecafab91baf42
Louis MENIGAULT <lomeniga@e2r4p8.42.fr>
2021-06-08 20:17:04 +0200

    almost finished

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <limits.h>
 #include "pipex.h"
 #include <string.h>
+#include <sys/types.h>
 
 extern char **(environ);
 
@@ -13,6 +14,26 @@ void	panic(void)
 {
 	perror(NULL);
 	exit(1);
+}
+
+int		panic_open(char *path, int flags) 
+{
+	int	fd;
+
+	fd = open(path, flags);
+	if (fd < 0)
+		panic();
+	return (fd);
+}
+
+int		create_file(char *path, int flags, int mode)
+{
+	int	fd;
+
+	fd = open(path, flags, mode);
+	if (fd < 0)
+		panic();
+	return (fd);
 }
 
 char	*find_path(void)
@@ -49,21 +70,45 @@ void	prep_exec(char *cmd, char **path)
 		free(str);
 		i++;
 	}
-	printf("%s\n", "command not found");
+	panic();
 }
 
-void	redirect(int in, int out, int fds[2])
+void	redirect(t_pair pair)
 {
-	dup2(out, 1);
-	dup2(in, 0);
-	close(fds[1]);
-	close(fds[0]);
+	dup2(pair.out, 1);
+	dup2(pair.in, 0);
+	close(pair.out);
+	close(pair.in);
+}
+
+void	spawn(t_pair pair, int fdclose, char *cmd, char **path)
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (!pid)
+	{
+		close(fdclose);
+		redirect(pair);
+		prep_exec(cmd, path);
+	}
+	if (pid < 0)
+		panic();
+	close(pair.in);
+	close(pair.out);
+	close(fdclose);
+}
+
+pid_t	wait_assign_in_control_structure(pid_t *pid)
+{
+	return (*pid = wait(NULL));
 }
 
 int	main(int ac, char **av)
 {
 	int		fds[2];
-	int		io[2];
+	int		fd;
+	int		pid;
 	char	**path;
 
 	if (ac < 5)
@@ -72,18 +117,11 @@ int	main(int ac, char **av)
 	if (!path)
 		panic();
 	pipe(fds);
-	if (!fork())
-	{
-		io[0] = open(av[1], O_RDONLY | O_CLOEXEC);
-		if (io[0] < 0)
-			panic();
-		redirect(io[0], fds[1], fds);
-		prep_exec(av[2], path);
-	}
-	io[1] = open(av[4], O_TRUNC | O_CLOEXEC | O_CREAT | O_WRONLY,
+	fd = panic_open(av[1], O_RDONLY | O_CLOEXEC);
+	spawn((t_pair){fd, fds[1]}, fds[0], av[2], path);
+	fd = create_file(av[4], O_TRUNC | O_CLOEXEC | O_CREAT | O_WRONLY,
 			S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-	if (io[1] < 0)
-		panic();
-	redirect(fds[0], io[1], fds);
-	prep_exec(av[3], path);
+	spawn((t_pair){fds[0], fd}, fds[1],  av[3], path);
+	while (wait_assign_in_control_structure(&pid) > 0)
+		(void)((void)"norme bullshit %d", pid);
 }
--- pipex.h
+++ pipex.h
@@ -6,4 +6,10 @@ char	**ft_split(char const *s, char c);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 char	*ft_strjoin(char const *s1, char const *s2);
 
+typedef struct s_pair
+{
+	int		in;
+	int		out;
+}				t_pair;
+
 #endif
